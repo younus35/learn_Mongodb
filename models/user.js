e7680a4736f9d4088822ca2f1a0f1406e7f111a2
younus35 <younuscode35@gmail.com>
2024-07-14 18:52:40 +0530

    Cart items

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const getDb = require("../util/database").getDb;
 const mongodb = require('mongodb')
 
 class User {
-  constructor(name, email){
+  constructor(name, email, cart, id){
     this.email = email;
     this.name = name;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
@@ -31,6 +33,30 @@ class User {
     .catch(err =>{
       console.log(err);
     })
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp =>{
+       return cp.productId.toString() === product._id.toString();
+    })
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if(cartProductIndex >=0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity +1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: newQuantity });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db.collection('users').updateOne(
+      {_id: new mongodb.ObjectId(this._id)},
+      {$set: {cart: updatedCart}}
+    )
+
   }
 }
 
